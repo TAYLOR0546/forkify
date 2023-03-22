7b74aeb70849bee74a03d94e642aa501dee16e8a
taylor0546 <imuranyusifsamu2000@gmail.com>
2023-03-22 18:46:49 +0000

    'Initial commit'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6417a5c8-7737-4188-881e-a52eb8af733a';
+export const MODAL_CLOSE_SEC = 2.5;
